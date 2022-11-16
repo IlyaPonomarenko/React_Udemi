d489b7ef9eaef2ce3c4ea57a68268683a97f4765
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-11-16 11:47:45 +0200

    routing

--- routed/src/App.jsx
+++ routed/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import BookList from "./pages/BookList";
 import Book from "./pages/Book";
+import NotFound from "./pages/NotFound";
+import BookLayout from "./BookLayout";
 
 class App extends Component {
   render() {
@@ -11,16 +13,22 @@ class App extends Component {
       <div>
         <nav>
           <ul>
-            <li><Link to="/"> Home </Link></li>
-            <li><Link to="/books"> Books </Link></li>
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/books"> Books </Link>
+            </li>
           </ul>
         </nav>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/books" element={<BookList />} />
-            <Route path="/books/:id" element={<Book />} />
-            
+            <Route path="/books" element={<BookLayout/>}>
+              <Route index element={<BookList />} />
+              <Route path=":id" element={<Book />} />
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
--- routed/src/pages/BookList.jsx
+++ routed/src/pages/BookList.jsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
 
 const BookList = () => {
   return (
-    <div>
       <h1>Book list</h1>
-      <Link to="/books/1">Book 1</Link>
-      <Link to="/books/2">Book 2</Link>
-    </div>
   );
 };
 export default BookList;
